f6b65d6fa14cb5813663fc9b4b63486ee3471c01
Sunset-kim <wood2002@naver.com>
2022-10-31 22:23:00 +0900

    feat: add order page test todo

---
+++ src/pages/OrderPage/tests/OrderPage.test.tsx
@@ -0,0 +1,29 @@
+describe("Order page", () => {
+  describe("when render", () => {
+    it.todo("fetch data");
+
+    it.todo("products price");
+
+    it.todo("All products count is 0");
+
+    it.todo("options price");
+
+    it.todo("All options checked is false");
+  });
+
+  describe("products increase", () => {
+    it.todo("each products");
+
+    it.todo("total price");
+  });
+
+  describe("products decrease", () => {
+    it.todo("0 to decrease");
+
+    it.todo("total price");
+  });
+
+  describe("option enable checked", () => {
+    it.todo("total price");
+  });
+});
