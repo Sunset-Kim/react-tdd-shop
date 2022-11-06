056587f9f7de897472ef8ae7c4b8d13eb267a35d
Sunset-kim <wood2002@naver.com>
2022-11-06 23:56:01 +0900

    add: jest context plugin

--- src/setupTests.js
+++ src/setupTests.js
@@ -4,6 +4,9 @@
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
 import { server } from "./mocks/server";
+import context from "jest-plugin-context";
+
+global.context = context;
 
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
