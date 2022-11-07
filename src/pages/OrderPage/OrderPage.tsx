e17dd1c4386851989e8019044ea3f70ef7fec500
Sunset-kim <wood2002@naver.com>
2022-11-08 05:36:45 +0900

    implement: total price

--- src/pages/OrderPage/OrderPage.tsx
+++ src/pages/OrderPage/OrderPage.tsx
@@ -8,6 +8,7 @@ import {
   ProductsStateCtx,
 } from "../../contexts/ProductsProvider";
 import { useContext } from "react";
+import { calcOptionPrice, calcProductsPice } from "../../util/calculate_price";
 
 function OrderPage() {
   const { updateCount, updateOption } = useContext(
@@ -15,10 +16,31 @@ function OrderPage() {
   ) as IProductsApi;
   const { products, options } = useContext(ProductsStateCtx) as ProductsState;
 
+  const productsPirce = calcProductsPice(products) ?? 0;
+  const optionsPrice = calcOptionPrice(options) ?? 0;
+
   return (
     <div>
       <Products products={products} onUpdateCount={updateCount} />
       <Options options={options} onUpdate={updateOption} />
+      <div>
+        <dl>
+          <div>
+            <dt>Prodcuts Price</dt>
+            <dd aria-label='product-price'>{`$ ${productsPirce}`}</dd>
+          </div>
+          <div>
+            <dt>Options Price</dt>
+            <dd aria-label='options-price'>{`$ ${optionsPrice}`}</dd>
+          </div>
+          <div>
+            <dt>Total Price</dt>
+            <dd aria-label='total-price'>{`$ ${
+              productsPirce + optionsPrice
+            }`}</dd>
+          </div>
+        </dl>
+      </div>
     </div>
   );
 }
