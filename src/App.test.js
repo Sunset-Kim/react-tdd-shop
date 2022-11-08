a09a9ca797f0b12c569553738500c725598c2a66
Sunset-kim <wood2002@naver.com>
2022-11-09 00:12:50 +0900

    remove: dead code

--- src/App.test.js
+++
@@ -1,7 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
-import App from "./App";
-
-test("count 0", () => {
-  render(<App />);
-});
